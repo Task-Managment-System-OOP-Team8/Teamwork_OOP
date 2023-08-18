16b1070e0b8d805da3cf0f3c7b07483b00508185
Karina Ivanova <carrie.ivanova93@gmail.com>
2023-08-18 09:43:15 +0300

    create FeedbackImplTests

--- TeamManagementSystem/test/models/FeedbackImplTests.java
+++ TeamManagementSystem/test/models/FeedbackImplTests.java
@@ -1,4 +1,65 @@
 package models;
 
+import com.company.oopTaskManagement.tasks.models.FeedbackImpl;
+import com.company.oopTaskManagement.tasks.models.enums.*;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import utils.TestUtilities;
+
 public class FeedbackImplTests {
+
+    private static final int TITLE_MIN_LENGTH = 10;
+    private static final int TITLE_MAX_LENGTH = 50;
+
+    private static final int DESCRIPTION_MIN_LENGTH = 10;
+    private static final int DESCRIPTION_MAX_LENGTH = 500;
+
+    private static final String VALID_TITLE = TestUtilities.getString(TITLE_MIN_LENGTH + 1);
+    private static final String INVALID_TITLE = TestUtilities.getString(TITLE_MAX_LENGTH + 1);
+    private static final String VALID_DESCRIPTION = TestUtilities.getString(DESCRIPTION_MIN_LENGTH + 1);
+
+    private static final String INVALID_DESCRIPTION = TestUtilities.getString(DESCRIPTION_MAX_LENGTH + 1);
+
+    @Test
+    public void constructor_Should_InitializeTitle_When_ArgumentsAreValid() {
+        FeedbackImpl feedback = getFeedback();
+        Assertions.assertEquals(VALID_TITLE, feedback.getTitle());
+    }
+
+    @Test
+    public void constructor_Should_InitializeDescription_When_ArgumentsAreValid() {
+        FeedbackImpl feedback = getFeedback();
+        Assertions.assertEquals(VALID_DESCRIPTION, feedback.getDescription());
+    }
+
+
+    @Test
+    public void constructor_Should_InitializeStatus_When_ArgumentsAreValid() {
+        Assertions.assertEquals(FeedbackEnums.NEW, getFeedback().getStatus());
+    }
+
+    @Test
+    public void constructor_Should_InitializeRating_When_ArgumentsAreValid(){
+        FeedbackImpl feedback = getFeedback();
+        Assertions.assertEquals(1,feedback.getRating());
+    }
+
+    @Test
+    public void constructor_Should_ThrowException_When_TitleIsInvalid() {
+        Assertions.assertNotEquals(INVALID_TITLE, VALID_TITLE);
+    }
+
+    @Test
+    public void constructor_Should_ThrowException_When_DescriptionIsInvalid() {
+        Assertions.assertNotEquals(INVALID_DESCRIPTION, VALID_DESCRIPTION);
+    }
+    @Test
+    public void constructor_Should_ThrowException_When_RatingIsInvalid(){
+        Assertions.assertNotEquals(0,getFeedback().getRating());
+    }
+
+    private FeedbackImpl getFeedback() {
+        return new FeedbackImpl(
+                VALID_TITLE,VALID_DESCRIPTION,1);
+    }
 }
